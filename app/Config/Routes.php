c1d436d0aed5ef7b33e84beec1a57031103fe382
khaidirakmal236 <khaidirakmal236@gmail.com>
2026-05-12 16:03:10 +0800

    aloha

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,5 +7,10 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/home', 'Home::index');
 $routes->get('/about-us', 'Home::about_us');
-$routes->get('/monkey', 'Home::monkey');
+$routes->get('/cafe', 'Home::cafe');
+$routes->get('/wisata', 'Home::wisata');
+$routes->get('/hidden-gems', 'Home::hidden_gems');
+$routes->get('/hidden-gems/(:any)', 'Home::hidden_gems/$1');
+
+
 
